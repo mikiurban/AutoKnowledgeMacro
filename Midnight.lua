ea1c558cf1eb3aec65336b09323424ba714f8319
Miki Urban <mikiurban@hotmail.com>
2026-03-24 22:33:47 -0700

    Added The War Within Profession Knowledge items

---
+++ Midnight.lua
@@ -0,0 +1,290 @@
+local _, T = ...
+local ALCHEMY_PK_ITEMS = {
+  -- MID Treasures
+  238532, -- Vial of Eversong Oddities
+  238533, -- Vial of Voidstorm Oddities
+  238534, -- Vial of Rootlands Oddities
+  238535, -- Vial of Zul'Aman Oddities
+  238536, -- Freshly Plucked Peacebloom
+  238537, -- Measured Ladle
+  238538, -- Pristine Potion
+  238539, -- Failed Experiment
+  -- Thalassian Treatise
+  245755, -- Thalassian Treatise on Alchemy
+  -- Work Orders
+  246320, -- Flicker of Midnight Alchemy Knowledge
+  246321, -- Glimmer of Midnight Alchemy Knowledge
+  -- Secret
+  255828, -- Sr. Professor Instructaur's Top Secret Guide to Midnight Alchemy
+  -- Chests
+  259188, -- Lightbloomed Spore Sample
+  259189, -- Aged Cruor
+  -- Faction Vendor
+  262645, -- Beyond the Event Horizon: Alchemy
+  -- Weekly quest  
+  263454, -- Thalassian Alchemist's Notebook
+}
+local BLACKSMITH_PK_ITEMS = {
+  -- MID Treasures
+  238540, -- Deconstructed Forge Techniques
+  238541, -- Silvermoon Smithing Kit
+  238542, -- Carefully Racked Spear
+  238543, -- Metalworking Cheat Sheet
+  238544, -- Voidstorm Defense Spear
+  238545, -- Rutaani Floratender's Sword
+  238546, -- Sin'dorei Master's Forgemace
+  238547, -- Silvermoon Blacksmith's Hammer
+  -- Thalassian Treatise
+  245763, -- Thalassian Treatise on Blacksmithing
+  -- Work Orders
+  246322, -- Flicker of Midnight Blacksmithing Knowledge
+  246323, -- Glimmer of Midnight Blacksmithing Knowledge
+  -- Secret
+  255829, -- Sr. Professor Instructaur's Top Secret Guide to Midnight Blacksmithing
+  -- Chests
+  259190, -- Thalassian Whestone
+  259191, -- Infused Quenching Oil
+  -- Faction Vendor
+  262644, -- Beyond the Event Horizon: Blacksmithing
+  -- Weekly Quest
+  263455, -- Thalassian Blacksmith's Journal
+}
+local ENCHANTING_PK_ITEMS = {
+  -- MID Treasures
+  238548, -- Enchanted Amani Mask
+  238549, -- Enchanted Sunfire Silk
+  238550, -- Pure Void Crystal
+  238551, -- Everblazing Sunmote
+  238552, -- Entropic Shard
+  238553, -- Primal Essence Orb
+  238554, -- Loa-Blessed Dust
+  238555, -- Sin'dorei Enchanting Rod
+  -- Thalassian Treatise
+  245759, -- Thalassian Treatise on Enchanting
+  -- Work Orders
+  246324, -- Flicker of Midnight Enchanting Knowledge
+  246325, -- Glimmer of Midnight Enchanting Knowledge
+  -- Chel the Chip
+  250445, -- Echo of Abundance: Enchanting 
+  -- Secret
+  255830, -- Sr. Professor Instructaur's Top Secret Guide to Midnight Enchanting
+  -- Faction Vendor
+  257600, -- Skill Issue: Enchanting
+  -- Chests
+  259192, -- Voidstorm Ashes
+  259193, -- Lost Thalassian Vellum
+  -- Weekly Quest
+  263464, -- Thalassian Enchanter's Folio
+  -- Disenchanting
+  267654, -- Swirling Arcane Essence
+  267655, -- Brimming Mana Shard
+}
+local ENGINEERING_PK_ITEMS = {
+  -- MID Treasures
+  238556, -- One Engineer's Junk
+  238557, -- Miniaturized Transport Skiff
+  238558, -- Manual of Mistakes and Mishaps
+  238559, -- Expeditious Pylon
+  238560, -- Ethereal Stormwrench
+  238561, -- Offline Helper Bot
+  238562, -- What To Do When Nothing Works
+  238563, -- Handy Wrench
+  -- Thalassian Treatise
+  245809, -- Thalassian Treatise on Engineering
+  -- Work Orders
+  246326, -- Flicker of Midnight Engineering Knowledge
+  246327, -- Glimmer of Midnight Engineering Knowledge
+  -- Secrets
+  255831, -- Sr. Professor Instructaur's Top Secret Guide to Midnight Engineering
+  -- Chests
+  259194, -- Dance Gear
+  259195, -- Dawn Capacitor
+  -- Faction Vendor
+  262646, -- Beyond the Event Horizon: Engineering
+  -- Weekly Quest
+  263456, -- Thalassian Engineer's Notepad 
+}
+local HERBALISM_PK_ITEMS = {
+  -- Weekly Profession Drops
+  238465, -- Thalassian Phoenix Plume
+  238466, -- Thalassian Phoenix Tail
+  238467, -- Thalassian Phoenix Ember
+  -- MID Treasures
+  238468, -- Bloomed Bud
+  238469, -- Sweeping Harvester's Scythe
+  238470, -- Simple Leaf Pruners
+  238471, -- Lightbloom Root
+  238472, -- A Spade
+  238473, -- Harvester's Sickle
+  238474, -- Peculiar Lotus
+  238475, -- Planting Shovel
+  -- Thalassian Treatise
+  245761, -- Thalassian Treatise on Herbalism
+  -- Vendor (Chel the Chip)
+  250443, -- Echo of Abundance: Herbalism
+  -- Secret
+  255832, -- Sr. Professor Instructaur's Top Secret Guide to Midnight Herbalism
+  -- Faction Vendor
+  258410, -- Traditions of the Haranir: Herbalism
+  -- Weekly quest
+  263462, -- Thalassian Herbalist's Notes
+}
+local INSCRIPTION_PK_ITEMS = {
+  -- MID Treasures
+  238572, -- Void-Touched Quill
+  238573, -- Leather-Bound Techniques
+  238574, -- Spare Ink
+  238575, -- Intrepid Explorer's Marker
+  238576, -- Leftover Sanguithorn Pigment
+  238577, -- Half-Baked Techniques
+  238578, -- Songwriter's Pen
+  238579, -- Songwriter's Quill
+  -- Thalassian Treatise
+  245757, -- Thalassian Treatise on Inscription
+  -- Work orders
+  246328, -- Flicker of Midnight Inscription Knowledge
+  246329, -- Glimmer of Midnight Inscription Knowledge
+  -- Secret
+  255833, -- Sr. Professor Instructaur's Top Secret Guide to Midnight Inscription
+  -- Faction Vendor
+  258411, -- Traditions of the Haranir: Inscription
+  -- Chests
+  259196, -- Brilliant Phoenix Ink
+  259197, -- Loa-Blessed Rune
+  -- Weekly quest
+  263457, -- Thalassian Scribe's Journal
+}
+local JEWELCRAFTING_PK_ITEMS = {
+  -- MID Treasures
+  238580, -- Sin'dorei Masterwork Chisel
+  238581, -- Speculative Voidstorm Crystal
+  238582, -- Dual-Function Magnifiers
+  238583, -- Poorly Rounded Vial
+  238584, -- Shattered Glass
+  238585, -- Vintage Soul Gem
+  238586, -- Ethereal Gem Pliers
+  238587, -- Sin'dorei Gem Faceters
+  -- Thalassian Treatise
+  245760, -- Thalassian Treatise on Jewelcrafting
+  -- Work Orders
+  246330, -- Flicker of Midnight Jewelcrafting Knowledge
+  246331, -- Glimmer of Midnight Jewelcrafting Knowledge
+  -- Secret
+  255834, -- Sr. Professor Instructaur's Top Secret Guide to Midnight Jewelcrafting
+  -- Faction Vendor
+  257599, -- Skill Issue: Jewelcrafting
+  -- Chests
+  259198, -- Void-Touched Eversong Diamond Fragments
+  259199, -- Harandar Stone Sample
+  -- Weekly Quest
+  263458, -- Thalassian Jewelcrafter's Notebook
+}
+local LEATHERWORKING_PK_ITEMS = {
+  -- MID Treasures  
+  238588, -- Amani Leatherworker's Tool
+  238589, -- Ethereal Leatherworking Knife
+  238590, -- Prestigiously Racked Hide
+  238591, -- Bundle of Tanner's Trinkets
+  238592, -- Patterns: Beyond the Void
+  238593, -- Haranir Leatherworking Mallet
+  238594, -- Haranir Leatherworking Knife
+  238595, -- Artisan's Considered Order
+  -- Thalassian Treatise
+  245758, -- Thalassian Treatise on Leatherworking
+  -- Work Orders
+  246332, -- Flicker of Midnight Leatherworking Knowledge
+  246333, -- Glimmer of Midnight Leatherworking Knowledge
+  -- Faction Vendor
+  250922, -- Whisper of the Loa: Leatherworking
+  -- Secret
+  255835, -- Sr. Professor Instructaur's Top Secret Guide to Midnight Leatherworking
+  -- Chests
+  259200, -- Amani Tanning Oil
+  259201, -- Thalassian Mana Oil
+  -- Weekly quest
+  263459, -- Thalassian Leatherworker's Journal
+}
+local MINING_PK_ITEMS = {
+  -- Weekly Profession Drops
+  237496, -- Igneous Rock Specimen
+  237506, -- Septarian Nodule
+  237507, -- Cloudy Quartz
+  -- MID Treasures
+  238596, -- Miner's Guide to Voidstorm
+  238597, -- Spelunker's Lucky Charm
+  238598, -- Lost Voidstorm Satchel
+  238599, -- Solid Ore Punchers
+  238600, -- Glimmering Void Pearl
+  238601, -- Amani Expert's Chisel
+  238602, -- Star Metal Deposit
+  238603, -- Spare Expedition Torch
+  -- Thalassian Treatise
+  245762, -- Thalassian Treatise on Mining
+  -- Vendor (Chel the Chip)
+  250444, -- Echo of Abundance: Mining
+  -- Faction Vendor
+  250924, -- Whisper of the Loa: Mining
+  -- Secret
+  255836, -- Sr. Professor Instructaur's Top Secret Guide to Midnight Mining
+  -- Weekly Quest
+  263463, -- Thalassian Miner's Notes
+}
+local SKINNING_PK_ITEMS = {
+  -- Profession drops
+  238625, -- Fine Void-Tempered Hide
+  238626, -- Mana-Infused Bone
+  238627, -- Manafused Sample
+  -- MID Treasures
+  238628, -- Lightbloom Afflicted Hide
+  238629, -- Cadre Skinning Knife
+  238630, -- Primal Hide
+  238631, -- Voidstorm Leather Sample
+  238632, -- Amani Tanning Oil
+  238633, -- Sin'dorei Tanning Oil
+  238634, -- Amani Skinning Knife
+  238635, -- Thalassian Skinning Knife
+  -- Thalassian Treatise
+  245828, -- Thalassian Treatise on Skinning
+  -- Vendor (Chel the Chip)
+  250360, -- Echo of Abundance: Skinning
+  250923, -- Whisper of the Loa: Skinning
+  -- Weekly Quest
+  263461, -- Thalassian Skinner's Notes
+}
+local TAILORING_PK_ITEMS = {
+  -- MID Treasures
+  238612, -- A Child's Stuffy
+  238613, -- A Really Nice Curtain
+  238614, -- Sin'dorei Outfitter's Ruler
+  238615, -- Wooden Weaving Sword
+  238616, -- Book of Sin'dorei Stitches
+  238617, -- Satin Throw Pillow
+  238618, -- Particularly Enchanting Tablecloth
+  238619, -- Artisan's Cover Comb
+  -- Thalassian Treatise
+  245756, -- Thalassian Treatise on Tailoring
+  -- Work Orders
+  246334, -- Flicker of Midnight Tailoring Knowledge
+  246335, -- Glimmer of Midnight Tailoring Knowledge
+  -- Secret
+  255838, -- Sr. Professor Instructaur's Top Secret Guide to Midnight Tailoring
+  -- Faction Vendor
+  257601, -- Skill Issue: Tailoring
+  -- Chests
+  259202, -- Embroidered Memento
+  259203, -- Finely Woven Lynx Collar
+  -- Weekly quest
+  263460, -- Thalassian Tailor's Notebook
+}
+
+T.professionMap[Enum.Profession.Alchemy][Enum.ExpansionLevel.Midnight] = ALCHEMY_PK_ITEMS
+T.professionMap[Enum.Profession.Blacksmithing][Enum.ExpansionLevel.Midnight] = BLACKSMITH_PK_ITEMS
+T.professionMap[Enum.Profession.Enchanting][Enum.ExpansionLevel.Midnight] = ENCHANTING_PK_ITEMS
+T.professionMap[Enum.Profession.Engineering][Enum.ExpansionLevel.Midnight] = ENGINEERING_PK_ITEMS
+T.professionMap[Enum.Profession.Herbalism][Enum.ExpansionLevel.Midnight] = HERBALISM_PK_ITEMS
+T.professionMap[Enum.Profession.Inscription][Enum.ExpansionLevel.Midnight] = INSCRIPTION_PK_ITEMS
+T.professionMap[Enum.Profession.Jewelcrafting][Enum.ExpansionLevel.Midnight] = JEWELCRAFTING_PK_ITEMS
+T.professionMap[Enum.Profession.Leatherworking][Enum.ExpansionLevel.Midnight] = LEATHERWORKING_PK_ITEMS
+T.professionMap[Enum.Profession.Mining][Enum.ExpansionLevel.Midnight] = MINING_PK_ITEMS
+T.professionMap[Enum.Profession.Skinning][Enum.ExpansionLevel.Midnight] = SKINNING_PK_ITEMS
+T.professionMap[Enum.Profession.Tailoring][Enum.ExpansionLevel.Midnight] = TAILORING_PK_ITEMS
---
+++ TheWarWithin.lua
@@ -0,0 +1,236 @@
+local _, T = ...
+local ALCHEMY_PK_ITEMS = {
+  222546, -- Algari Treatise on Alchemy
+  224024, -- Theories of Bodily Transmutation, Chapter 8
+  224645, -- Jewel-Etched Alchemy Notes
+  226265, -- Earthen Iron Powder
+  226266, -- Metal Dornogal Frame
+  226267, -- Reinforced Beaker
+  226268, -- Engraved Stirring Rod
+  226269, -- Chemist's Purified Water
+  226270, -- Sanctified Mortar and Pestle
+  226271, -- Nerubian Mixing Salts
+  226272, -- Dark Apothecary's Vial
+  227409, -- Faded Alchemist's Research
+  227420, -- Exceptional Alchemist's Research
+  227431, -- Pristine Alchemist's Research
+  228724, -- Flicker of Alchemy Knowledge
+  228725, -- Glimmer of Alchemy Knowledge
+  232499, -- Undermine Treatise on Alchemy
+  235865, -- Ethereal Tome of Alchemy Knowledge
+}
+local BLACKSMITH_PK_ITEMS = {
+  222554, -- Algari Treatise on Blacksmithing
+  224038, -- Smithing After Saronite
+  224647, -- Jewel-Etched Blacksmithing Notes
+  226276, -- Ancient Earthen Anvil
+  226277, -- Dornogal Hammer
+  226278, -- Ringing Hammer Vise
+  226279, -- Earthen Chisels
+  226280, -- Holy Flame Forge
+  226281, -- Radiant Tongs
+  226282, -- Nerubian Smith's Kit
+  226283, -- Spiderling's Wire Brush
+  227407, -- Faded Blacksmith's Diagrams
+  227418, -- Exceptional Blacksmith's Diagrams
+  227429, -- Pristine Blacksmith's Diagrams
+  228726, -- Flicker of Blacksmithing Knowledge
+  228727, -- Glimmer of Blacksmithing Knowledge
+  232500, -- Undermine Treatise on Blacksmithing
+  235864, -- Ethereal Tome of Blacksmithing Knowledge
+}
+local ENCHANTING_PK_ITEMS = {
+  222550, -- Algari Treatise on Enchanting
+  224050, -- Web Sparkles: Pretty and Powerful
+  224652, -- Jewel-Etched Enchanting Notes
+  226284, -- Grinded Earthen Gem
+  226285, -- Silver Dornogal Rod
+  226286, -- Soot-Coated Orb
+  226287, -- Animated Enchanting Dust
+  226288, -- Essence of Holy Fire
+  226289, -- Enchanted Arathi Scroll
+  226290, -- Book of Dark Magic
+  226291, -- Void Shard
+  227411, -- Faded Enchanter's Research
+  227422, -- Exceptional Enchanter's Research
+  227433, -- Pristine Enchanter's Research
+  227659, -- Fleeting Arcane Manifestation
+  227661, -- Gleaming Telluric Crystal
+  227662, -- Shimmering Dust
+  228728, -- Flicker of Enchanting Knowledge
+  228729, -- Glimmer of Enchanting Knowledge
+  232501, -- Undermine Treatise on Enchanting
+  235863, -- Ethereal Tome of Enchanting Knowledge
+}
+local ENGINEERING_PK_ITEMS = {
+  222621, -- Algari Treatise on Engineering
+  224052, -- Clocks, Gears, Sprockets, and Legs
+  224653, -- Machine-Learned Engineering Notes
+  226292, -- Rock Engineer's Wrench
+  226293, -- Dornogal Spectacles
+  226294, -- Inert Mining Bomb
+  226295, -- Earthen Construct Blueprints
+  226296, -- Holy Firework Dud
+  226297, -- Arathi Safety Gloves
+  226298, -- Puppeted Mechanical Spider
+  226299, -- Emptied Venom Canister
+  227412, -- Faded Engineer's Scribblings
+  227423, -- Exceptional Engineer's Scribblings
+  227434, -- Pristine Engineer's Scribblings
+  228730, -- Flicker of Engineering Knowledge
+  228731, -- Glimmer of Engineering Knowledge
+  232507, -- Undermine Treatise on Engineering
+  235862, -- Ethereal Tome of Engineering Knowledge
+}
+local HERBALISM_PK_ITEMS = {
+  222552, -- Algari Treatise on Herbalism
+  224023, -- Herbal Embalming Techniques
+  224264, -- Deepgrove Petal
+  224265, -- Deepgrove Rose
+  224656, -- Void-Lit Herbalism Notes
+  226300, -- Ancient Flower
+  226301, -- Dornogal Gardening Scythe
+  226302, -- Earthen Digging Fork
+  226303, -- Fungarian Slicer's Knife
+  226304, -- Arathi Garden Trowel
+  226305, -- Arathi Herb Pruner
+  226306, -- Web-Entangled Lotus
+  226307, -- Tunneler's Shovel
+  227415, -- Faded Herbalist's Notes
+  227426, -- Exceptional Herbalist's Notes
+  227437, -- Pristine Herbalist's Notes
+  232503, -- Undermine Treatise on Herbalism
+  235861, -- Ethereal Tome of Herbalism Knowledge
+}
+local INSCRIPTION_PK_ITEMS = {
+  222548, -- Algari Treatise on Inscription
+  224053, -- Eight Views on Defense against Hostile Runes
+  224654, -- Machine-Learned Inscription Notes
+  226308, -- Dornogal Scribe's Quill
+  226309, -- Historian's Dip Pen
+  226310, -- Runic Scroll
+  226311, -- Blue Earthen Pigment
+  226312, -- Informant's Fountain Pen
+  226313, -- Calligrapher's Chiseled Marker
+  226314, -- Nerubian Texts
+  226315, -- Venomancer's Ink Well
+  227408, -- Faded Scribe's Runic Drawings
+  227419, -- Exceptional Scribe's Runic Drawings
+  227430, -- Pristine Scribe's Runic Drawings
+  228732, -- Flicker of Inscription Knowledge
+  228733, -- Glimmer of Inscription Knowledge
+  232508, -- Undermine Treatise on Inscription
+  235860, -- Ethereal Tome of Inscription Knowledge
+}
+local JEWELCRAFTING_PK_ITEMS = {
+  222551, -- Algari Treatise on Jewelcrafting
+  224054, -- Emergent Crystals of the Surface-Dwellers
+  224655, -- Void-Lit Jewelcrafting Notes
+  226316, -- Gentle Jewel Hammer
+  226317, -- Earthen Gem Pliers
+  226318, -- Carved Stone File
+  226319, -- Jeweler's Delicate Drill
+  226320, -- Arathi Sizing Gauges
+  226321, -- Librarian's Magnifiers
+  226322, -- Ritual Caster's Crystal
+  226323, -- Nerubian Bench Blocks
+  227413, -- Faded Jeweler's Illustrations
+  227424, -- Exceptional Jeweler's Illustrations
+  227435, -- Pristine Jeweler's Illustrations
+  228734, -- Flicker of Jewelcrafting Knowledge
+  228735, -- Glimmer of Jewelcrafting Knowledge
+  232504, -- Undermine Treatise on Jewelcrafting
+  235859, -- Ethereal Tome of Jewelcrafting Knowledge
+}
+local LEATHERWORKING_PK_ITEMS = {
+  222549, -- Algari Treatise on Leatherworking
+  224056, -- Uses for Leftover Husks (After You Take Them Apart)
+  224658, -- Void-Lit Leatherworking Notes
+  226324, -- Earthen Lacing Tools
+  226325, -- Dornogal Craftsman's Flat Knife
+  226326, -- Underground Stropping Compound
+  226327, -- Earthen Awl
+  226328, -- Arathi Beveler Set
+  226329, -- Arathi Leather Burnisher
+  226330, -- Nerubian Tanning Mallet
+  226331, -- Curved Nerubian Skinning Knife
+  227414, -- Faded Leatherworker's Diagrams
+  227425, -- Exceptional Leatherworker's Diagrams
+  227436, -- Pristine Leatherworker's Diagrams
+  228736, -- Flicker of Leatherworking Knowledge
+  228737, -- Glimmer of Leatherworking Knowledge
+  232505, -- Undermine Treatise on Leatherworking
+  235858, -- Ethereal Tome of Leatherworking Knowledge
+}
+local MINING_PK_ITEMS = {
+  222553, -- Algari Treatise on Mining
+  224055, -- A Rocky Start
+  224583, -- Slab of Slate
+  224584, -- Erosion-Polished Slate
+  224651, -- Machine-Learned Mining Notes
+  226332, -- Earthen Miner's Gavel
+  226333, -- Dornogal Chisel
+  226334, -- Earthen Excavator's Shovel
+  226335, -- Regenerating Ore
+  226336, -- Arathi Precision Drill
+  226337, -- Devout Archaeologist's Excavator
+  226338, -- Heavy Spider Crusher
+  226339, -- Nerubian Mining Supplies
+  227416, -- Faded Miner's Notes
+  227427, -- Exceptional Miner's Notes
+  227438, -- Pristine Miner's Notes
+  232509, -- Undermine Treatise on Mining
+  235857, -- Ethereal Tome of Mining Knowledge
+}
+local SKINNING_PK_ITEMS = {
+  222649, -- Algari Treatise on Skinning
+  224007, -- Uses for Leftover Husks (How to Take Them Apart)
+  224657, -- Void-Lit Skinning Notes
+  224780, -- Toughened Tempest Pelt
+  224781, -- Abyssal Fur
+  226340, -- Dornogal Carving Knife
+  226341, -- Earthen Worker's Beams
+  226342, -- Artisan's Drawing Knife
+  226343, -- Fungarian's Rich Tannin
+  226344, -- Arathi Tanning Agent
+  226345, -- Arathi Craftsman's Spokeshave
+  226346, -- Nerubian's Slicking Iron
+  226347, -- Carapace Shiner
+  227417, -- Faded Skinner's Notes
+  227428, -- Exceptional Skinner's Notes
+  227439, -- Pristine Skinner's Notes
+  232506, -- Undermine Treatise on Skinning
+  235856, -- Ethereal Tome of Skinning Knowledge
+}
+local TAILORING_PK_ITEMS = {
+  222547, -- Algari Treatise on Tailoring
+  224036, -- And That's A Web-Wrap!
+  224648, -- Jewel-Etched Tailoring Notes
+  226348, -- Dornogal Seam Ripper
+  226349, -- Earthen Tape Measure
+  226350, -- Runed Earthen Pins
+  226351, -- Earthen Stitcher's Snips
+  226352, -- Arathi Rotary Cutter
+  226353, -- Royal Outfitter's Protractor
+  226354, -- Nerubian Quilt
+  226355, -- Nerubian's Pincushion
+  227410, -- Faded Tailor's Diagrams
+  227421, -- Exceptional Tailor's Diagrams
+  227432, -- Pristine Tailor's Diagrams
+  228738, -- Flicker of Tailoring Knowledge
+  228739, -- Glimmer of Tailoring Knowledge
+  232502, -- Undermine Treatise on Tailoring
+  235855, -- Ethereal Tome of Tailoring Knowledge
+}
+
+T.professionMap[Enum.Profession.Alchemy][Enum.ExpansionLevel.WarWithin] = ALCHEMY_PK_ITEMS
+T.professionMap[Enum.Profession.Blacksmithing][Enum.ExpansionLevel.WarWithin] = BLACKSMITH_PK_ITEMS
+T.professionMap[Enum.Profession.Enchanting][Enum.ExpansionLevel.WarWithin] = ENCHANTING_PK_ITEMS
+T.professionMap[Enum.Profession.Engineering][Enum.ExpansionLevel.WarWithin] = ENGINEERING_PK_ITEMS
+T.professionMap[Enum.Profession.Herbalism][Enum.ExpansionLevel.WarWithin] = HERBALISM_PK_ITEMS
+T.professionMap[Enum.Profession.Inscription][Enum.ExpansionLevel.WarWithin] = INSCRIPTION_PK_ITEMS
+T.professionMap[Enum.Profession.Jewelcrafting][Enum.ExpansionLevel.WarWithin] = JEWELCRAFTING_PK_ITEMS
+T.professionMap[Enum.Profession.Leatherworking][Enum.ExpansionLevel.WarWithin] = LEATHERWORKING_PK_ITEMS
+T.professionMap[Enum.Profession.Mining][Enum.ExpansionLevel.WarWithin] = MINING_PK_ITEMS
+T.professionMap[Enum.Profession.Skinning][Enum.ExpansionLevel.WarWithin] = SKINNING_PK_ITEMS
+T.professionMap[Enum.Profession.Tailoring][Enum.ExpansionLevel.WarWithin] = TAILORING_PK_ITEMS
